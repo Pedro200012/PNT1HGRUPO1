ca91c5f4e37e7ad98cfc2fc3356a855fd929c297
pedroKahane <pedrok@datosclaros.com>
2022-11-10 18:18:28 -0300

    edit delete detail

---
+++ concesionaria_v2/MVC-CONCESIONARIA/MVCConcesionaria/MVCConcesionaria/Context/ConcesionariaDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCConcesionaria.Models;
+namespace MVCConcesionaria.Context
+{
+    public class ConcesionariaDatabaseContext : DbContext
+
+    {
+        public
+        ConcesionariaDatabaseContext(DbContextOptions<ConcesionariaDatabaseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Auto> Autos { get; set; }
+        public DbSet<Moto> Motos { get; set; }
+        public DbSet<Camioneta> Camionetas { get; set; }
+
+
+    }
+}
---
+++ concesionaria_v2/MVC-CONCESIONARIA/MVCConcesionaria/MVCConcesionaria/Migrations/20221024162453_Inicial.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCConcesionaria.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(nullable: true),
+                    modelo = table.Column<string>(nullable: true),
+                    esUsado = table.Column<bool>(nullable: false),
+                    cantKm = table.Column<double>(nullable: false),
+                    cantPuertas = table.Column<int>(nullable: false),
+                    ImageName = table.Column<string>(nullable: true),
+                    PhotoFile = table.Column<byte[]>(nullable: true),
+                    ImageMimeType = table.Column<string>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autos", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Camionetas",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(nullable: true),
+                    modelo = table.Column<string>(nullable: true),
+                    esUsado = table.Column<bool>(nullable: false),
+                    cantKm = table.Column<double>(nullable: false),
+                    es4x4 = table.Column<bool>(nullable: false),
+                    esDobleCabina = table.Column<bool>(nullable: false),
+                    ImageName = table.Column<string>(nullable: true),
+                    PhotoFile = table.Column<byte[]>(nullable: true),
+                    ImageMimeType = table.Column<string>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Camionetas", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Motos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(nullable: true),
+                    modelo = table.Column<string>(nullable: true),
+                    esUsado = table.Column<bool>(nullable: false),
+                    cantKm = table.Column<double>(nullable: false),
+                    Tipo = table.Column<int>(nullable: false),
+                    ImageName = table.Column<string>(nullable: true),
+                    PhotoFile = table.Column<byte[]>(nullable: true),
+                    ImageMimeType = table.Column<string>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Motos", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autos");
+
+            migrationBuilder.DropTable(
+                name: "Camionetas");
+
+            migrationBuilder.DropTable(
+                name: "Motos");
+        }
+    }
+}
